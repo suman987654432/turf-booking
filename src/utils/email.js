556e3a4682c343f229da8811f70a15717da0b3e2
Suman Kumar <sumanqaj9876@gmail.com>
2026-09-10 17:37:45 +0530

    feat: implement email service with IPv4 DNS resolution for account verification

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,4 +1,8 @@
 const nodemailer = require('nodemailer');
+const dns = require('dns');
+
+// Force IPv4 DNS resolution to fix Render ENETUNREACH IPv6 errors
+dns.setDefaultResultOrder('ipv4first');
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
